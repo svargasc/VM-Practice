42b61541abff6e76d2e1900760f867edb34364f5
svargasc <svc.vargas03@gmail.com>
2023-06-07 07:17:30 -0500

    added bash modified

--- bash/update_repos.sh
+++ bash/update_repos.sh
@@ -3,11 +3,11 @@
 PATH="C:\Users\SERGIO VARGAS\Documents\DevelopeR-Practics\VM-Practice"
 echo "$PATH"
 
-git clone https://github.com/svargasc/VM-Store.git $PATH
-cd "$PATH\VM-Store"
+git clone https://github.com/svargasc/VM-Practice.git $PATH
+cd "$PATH\VM-Practice"
 git checkout master 
 
 cd ..
-git clone https://github.com/svargasc/VM-Store.git $PATH
-cd "$PATH\VM-Store"
+git clone https://github.com/svargasc/VM-Practice.git $PATH
+cd "$PATH\VM-Practice"
 git checkout master
